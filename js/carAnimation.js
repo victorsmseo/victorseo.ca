eb3326756466f3f72e19564d309de6956e4ffb14
victorsmseo <victorsmseo@gmail.com>
2018-01-25 18:42:34 -0800

    Started carAnimation

--- js/carAnimation.js
+++ js/carAnimation.js
@@ -2,6 +2,6 @@ var canvas;
 var ctx;
 window.onload = init;
 function init() {
-    canvas = document.getElementById("canvas");
+    canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
 }
