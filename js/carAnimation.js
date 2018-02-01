c907b4ae20dab7eedd4a1a7ca5bb620ab5435edd
victorsmseo <victorsmseo@gmail.com>
2018-02-01 15:33:00 -0800

    Finished front tire animation

--- js/carAnimation.js
+++ js/carAnimation.js
@@ -24,7 +24,7 @@ function animationLoop(timestamp) {
     lanePosition %= canvas.width/2;
     carPosition += 1;
     wheelPosition += 1;
-    wheelPosition %= 40;
+    wheelPosition %= 20;
     requestId = requestAnimationFrame(animationLoop);
 }
 function drawBackground() {
@@ -47,30 +47,29 @@ function car() {
     rotateTire(wheelPosition);
 }
 function rotateTire(wheelPosition) {
-    if ((wheelPosition >= 0) && (wheelPosition < 10)) {
-        // ctx.save();
+    if ((wheelPosition >= 0) && (wheelPosition < 5)) {
         tire1(5,5,75,86);
-        // tire2();
+        tire2(5,5,75,86);
+        tire3(5,5,75,86);
+        tire4(5,5,75,86);
+        ctx.restore();
+    } else if (wheelPosition < 10) {
+        tire1(5,-5,75,-98);
+        tire2(-5,5,-80,86);
+        tire3(-5,5,-80,86);
+        tire4(5,-5,75,-98);
+        ctx.restore();
+    } else if (wheelPosition < 15) {
+        tire1(-5,-5,-80,-98);
+        tire2(-5,-5,-80,-98);
+        tire3(-5,-5,-80,-98);
+        tire4(-5,-5,-80,-98);
         ctx.restore();
     } else if (wheelPosition < 20) {
-        // ctx.save();
-        // ctx.scale(5,-5);
-        // ctx.translate(75,-98);
-        tire1(5,-5,75,-98);
-        // tire2();
-        ctx.restore();
-    } else if (wheelPosition < 30) {
-        // ctx.save();
-        // ctx.scale(-5,-5);
-        // ctx.translate(-80,-98);
-        tire1(-5,-5,-80,-98);
-        // tire2();
-        ctx.restore();
-    } else if (wheelPosition < 40) {
-        // ctx.save();
-        // ctx.scale(-5,5);
-        // ctx.translate(-80,86);
         tire1(-5,5,-80,86);
+        tire2(5,-5,75,-98);
+        tire3(5,-5,75,-98);
+        tire4(-5,5,-80,86);
         ctx.restore();
     }
 }
@@ -122,26 +121,67 @@ function tire2(scaleX, scaleY, translateX, translateY) {
     ctx.fillRect(4,4,1,1);
     ctx.fillStyle = colorGreyLight3;
     ctx.fillRect(3,5,1,1);
+
+    ctx.restore();
 }
 function tire3(scaleX, scaleY, translateX, translateY) {
     const colorGrey = "#373737";
     const colorGreyLight1 = "#4F4F4F";
     const colorGreyLight2 = "#7F7F7F";
+    const colorGreyLight3 = "#989898";
+
+    ctx.save();
+    ctx.scale(scaleX,scaleY);
+    ctx.translate(translateX,translateY);
+
     ctx.fillStyle = "white";
+    ctx.fillRect(0,10,1,2);
     ctx.fillStyle = "black";
+    ctx.fillRect(1,8,1,2);
     ctx.fillStyle = colorGrey;
+    ctx.fillRect(1,6,1,1);
     ctx.fillStyle = colorGreyLight1;
+    ctx.fillRect(0,6,1,2);
     ctx.fillStyle = colorGreyLight2;
+    ctx.fillRect(1,7,1,1);
+    ctx.fillRect(0,8,1,2);
+    ctx.fillRect(1,11,1,1);
+    ctx.fillStyle = colorGreyLight3;
+    ctx.fillRect(1,10,1,1);
+
+    ctx.restore();
 }
 function tire4(scaleX, scaleY, translateX, translateY) {
     const colorGrey = "#373737";
     const colorGreyLight1 = "#4F4F4F";
     const colorGreyLight2 = "#7F7F7F";
+    const colorGreyLight3 = "#989898";
+    const colorGreyLight4 = "#BCBCBC";
+
+    ctx.save();
+    ctx.scale(scaleX,scaleY);
+    ctx.translate(translateX,translateY);
+
     ctx.fillStyle = "white";
+    ctx.fillRect(4,11,1,1);
     ctx.fillStyle = "black";
-    ctx.fillStyle = colorGrey;
+    ctx.fillRect(2,6,1,1);
     ctx.fillStyle = colorGreyLight1;
+    ctx.fillRect(4,6,1,2);
+    ctx.fillRect(2,7,1,1);
     ctx.fillStyle = colorGreyLight2;
+    ctx.fillRect(3,6,1,2);
+    ctx.fillRect(2,8,1,1);
+    ctx.fillRect(4,8,1,3);
+    ctx.fillRect(3,11,1,1);
+    ctx.fillStyle = colorGreyLight3;
+    ctx.fillRect(3,8,1,2);
+    ctx.fillRect(2,10,1,2);
+    ctx.fillStyle = colorGreyLight4;
+    ctx.fillRect(2,9,1,1);
+    ctx.fillRect(3,10,1,1);
+
+    ctx.restore();
 }
 function drawAsphalt(x) {
     const colorYellow = "#EEC830";
